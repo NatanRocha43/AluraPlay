585c7c5302546fa84b5a6e4d7bea14c99fd87944
Natan Rocha <natan.rocha@codeandsoul.com>
2024-03-06 15:13:43 -0300

    Added a method to add videos

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -1,10 +1,10 @@
 import { conectaApi } from "./conectaApi.js";
 
-const lista = document.querySelector('[data-lista]')
+const lista = document.querySelector("[data-lista]");
 
 function constroiCard(titulo, descricao, url, imagem) {
-    const video = document.createElement('li')
-    video.className = 'videos__item'
+    const video = document.createElement("li");
+    video.className = "videos__item";
     video.innerHTML = `
         <iframe width="100%" height="72%" src="${url}"
         title="${titulo}" frameborder="0"
@@ -14,13 +14,15 @@ function constroiCard(titulo, descricao, url, imagem) {
             <img src="${imagem}" alt="logo canal alura">
             <h3>${titulo}</h3>
             <p>${descricao}</p>
-        </div>
-    `
-    return video
+        </div>`
+
+    return video;
 }
 
-async function listaVideo() {
-    const listaApi = await conectaApi.listaVideos()
-    listaApi.forEach(element => lista.appendChild(constroiCard(element.titulo, element.descricao, element.url, element.imagem)));
+async function listaVideos() {
+    const listaApi = await conectaApi.listaVideos();
+    listaApi.forEach(elemento => lista.appendChild(
+        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
 }
-listaVideo()
+
+listaVideos();
